5680610dd162c8bcedf17e965781fbf21926c6b0
kristigrembi <103140834+kristigrembi@users.noreply.github.com>
2022-05-17 13:22:01 +0100

    # commit

--- testingPython/testing.py
+++ testingPython/testing.py
@@ -1,19 +1,10 @@
-import random
-
-print('Welcome to Python!')
-
-print('This is a loop printing 5 times')
-for x in range(1, 6):
-    print(f'x is: {x}')
-
-weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-day = random.choice(weekdays)
-
-print(f'Today is: {day}')
-
-if day == 'Monday':
-    print('It will be a long week!')
-elif(day == 'Friday'):
-    print('Woohoo, time for the weekend!')
-else:
-    print('Not quite there yet.')
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
